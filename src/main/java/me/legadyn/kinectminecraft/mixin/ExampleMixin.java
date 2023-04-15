0064b0ffb3df84c7c35ec37b70c8971ed8ca6439
Legadyn <legadyn@gmail.com>
2023-04-15 15:12:27 -0300

    Initial commit

---
+++ src/main/java/me/legadyn/kinectminecraft/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package me.legadyn.kinectminecraft.mixin;
+
+import me.legadyn.kinectminecraft.KinectMinecraft;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		KinectMinecraft.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
